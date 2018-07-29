61fa014410d84e70e4a47fc616c27a6e6a303cd7
Miguel <miguelcudaihl@gmail.com>
2018-07-29 11:59:54 +0800

    Completed linear block ID generator

---
+++ src/CodeSnippets.Tests/RdbmsLinearBlockIdGenerator/LinearBlockIdGeneratorForMssqlTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using CodeSnippets.RdbmsLinearBlockIdGenerator;
+using Xunit;
+using System.Collections;
+using System.Collections.Generic;
+using Shouldly;
+
+namespace CodeSnippets.Tests.RdbmsLinearBlockIdGenerator
+{
+    public class LinearBlockIdGeneratorForMssqlTests : IDisposable
+    {
+        public static string ConnectionString => Environment.GetEnvironmentVariable("SQLSERVER_CONNECTION_STRING") ?? @"Data Source=.;Initial Catalog=tempdb;User Id=sa;Password=Asdfasdf1";
+        const string TableName = "LinearBlockTestTable";
+
+        static Func<DbConnection> _dbConnectionFactory = () => new SqlConnection(ConnectionString);
+        
+        public LinearBlockIdGeneratorForMssqlTests()
+        {
+            Cleanup();
+            Initialize();
+        }
+
+        [Fact]
+        public void Should_create_dimension_and_increment_range_if_exceeded()
+        {
+            var generator = new LinearBlockIdGenerator(_dbConnectionFactory, range: 15, tableName: TableName);
+            var resultStore = new List<long>();
+            for (int ctr = 0; ctr < 20; ctr++)
+            {
+                resultStore.Add(generator.GetNextId("a"));
+            }
+
+            resultStore.Count.ShouldBe(20);
+            GetDimensionNextValInDb("a").ShouldBe(31);
+        }
+
+
+        public void Dispose()
+        {
+            Cleanup();
+        }
+
+        private void Cleanup()
+        {
+            using (var c = _dbConnectionFactory())
+            using (var cmd = c.CreateCommand())    
+            {
+                c.Open();
+                cmd.CommandText = $"IF OBJECT_ID('dbo.{TableName}', 'U') IS NOT NULL DROP TABLE dbo.{TableName};";
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void Initialize()
+        {
+            using (var c = _dbConnectionFactory())
+            using (var cmd = c.CreateCommand())
+            {
+                c.Open();
+                cmd.CommandText = $"CREATE TABLE {TableName} (" +
+                    "              dimension VARCHAR(20) NOT NULL," +
+                    "              nextval int NOT NULL);";
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private int GetDimensionNextValInDb(string dimension)
+        {
+            using (var c = _dbConnectionFactory())
+            using (var cmd = c.CreateCommand())
+            {
+                c.Open();
+                cmd.CommandText = $"SELECT nextval FROM dbo.{TableName} WHERE dimension=@dimension";
+                var param = cmd.CreateParameter();
+                param.Value = dimension;
+                param.ParameterName = "@dimension";
+                cmd.Parameters.Add(param);
+                return (int)cmd.ExecuteScalar();
+            }
+        }
+    }
+}
--- src/RdbmsLinearBlockIdGenerator/LinearBlockIdGenerator.cs
+++ src/RdbmsLinearBlockIdGenerator/LinearBlockIdGenerator.cs
@@ -1,40 +1,68 @@
 using System;
+using System.Collections.Concurrent;
 using System.Data;
+using System.Data.Common;
+using System.Threading;
 
-
-namespace RdbmsLinearBlockIdGenerator
+namespace CodeSnippets.RdbmsLinearBlockIdGenerator
 {
     /// <summary>
-    /// This class manages a linear identifiers block allocator
-    /// c.f., http://literatejava.com/hibernate/linear-block-allocator-a-superior-alternative-to-hilo/
     /// This snippet is largely based off the implementation in https://github.com/sebastienros/yessql
+    /// Literature at http://literatejava.com/hibernate/linear-block-allocator-a-superior-alternative-to-hilo/
     /// </summary>
     public class LinearBlockIdGenerator
     {
         public readonly int MaxRetries = 20;
         private readonly int _range;
-        private readonly string _tableName;
-        private readonly string _tablePrefix;
+        private readonly ConcurrentDictionary<string, object> _lockChest = new ConcurrentDictionary<string, object>();
+        private readonly Func<DbConnection> _dbConnectionFactory;
         private bool _initialized;
         private long _start;
         private int _increment;
         private long _end;
-        
+        private string _selectSql;
+        private string _updateSql;
+        private string _insertSql;
 
-        public LinearBlockIdGenerator(string schema, int range=100, string tableName="Identifiers")
+
+        public LinearBlockIdGenerator(Func<DbConnection> dbConnectionFactory, int range = 20, string schema = "dbo", string tableName = "Identifiers")
         {
-            _tableName = tableName ?? "Identifiers";
-            _range = range ?? 100
-            _tablePrefix = _tablePrefix;
+            _dbConnectionFactory = dbConnectionFactory
+                ?? throw new ArgumentNullException(nameof(dbConnectionFactory));
+            _range = range;
+
+            // set SQL commands. There must be a better way to apply RDBMS specific quotes without using preprocessors
+#if MSSQL
+            _selectSql = string.Format("SELECT [nextval] FROM [{0}].[{1}] WHERE dimension = @dimension;", schema, tableName);
+            _updateSql = string.Format("UPDATE [{0}].[{1}] SET [nextval] = @new WHERE [nextval] = @previous AND [dimension] = @dimension;", schema, tableName);
+            _insertSql = string.Format("INSERT INTO [{0}].[{1}] ([dimension], [nextval]) VALUES(@dimension, @nextval);", schema, tableName);
+#elif MYSQL
+            _selectSql = string.Format("SELECT `nextval` FROM `{0}`.`{1}` WHERE dimension = @dimension;", schema, tableName);
+            _updateSql = string.Format("UPDATE `{0}`.`{1}` SET `nextval` = @new WHERE `nextval` = @previous AND `dimension` = @dimension;", schema, tableName);
+            _insertSql = string.Format("INSERT INTO `{0}`.`{1}` (`dimension`, `nextval`) VALUES(@dimension, @nextval);", schema, tableName);
+#elif PG
+            _selectSql = string.Format("SELECT \"nextval\" FROM \"{0}\".\"{1}\" WHERE dimension = @dimension;", schema, tableName);
+            _updateSql = string.Format("UPDATE \"{0}\".\"{1}\" SET \"nextval\" = @new WHERE \"nextval\" = @previous AND \"dimension\" = @dimension;", schema, tableName);
+            _insertSql = string.Format("INSERT INTO \"{0}\".\"{1}\" (\"dimension\", \"nextval\") VALUES(@dimension, @nextval);", schema, tableName);
+#else
+            _selectSql = string.Format("SELECT nextval FROM {0}.{1} WHERE dimension = @dimension;", schema, tableName);
+            _updateSql = string.Format("UPDATE {0}.{1} SET nextval = @new WHERE nextval = @previous AND dimension = @dimension;", schema, tableName);
+            _insertSql = string.Format("INSERT INTO {0}.{1} (dimension, nextval) VALUES(@dimension, @nextval);", schema, tableName);
+                                       
+#endif
         }
 
-        public long GetNextId(ISession session, string dimension)
+        public long GetNextId(string dimension)
         {
             // Initialize the range
             if (_end == 0)
             {
-                EnsureInitialized(session, dimension);
-                LeaseRange(session, dimension);
+                using (var dbConnection = _dbConnectionFactory())
+                {
+                    dbConnection.Open();
+                    EnsureInitialized(dbConnection, dimension);
+                    LeaseRange(dbConnection, dimension);
+                }
             }
 
             var newIncrement = Interlocked.Increment(ref _increment);
@@ -42,74 +70,85 @@ namespace RdbmsLinearBlockIdGenerator
 
             if (nextId > _end)
             {
-                LeaseRange(session, dimension);
-                return GetNextId(session, dimension);
+                using (var dbConnection = _dbConnectionFactory())
+                {
+                    dbConnection.Open();
+                    LeaseRange(dbConnection, dimension);
+                    return GetNextId(dimension);
+                }
             }
 
             return nextId;
         }
 
-        private void LeaseRange(ISession session, string dimension)
+        private void LeaseRange(DbConnection dbConnection, string dimension)
         {
-            lock (this)
+            lock (_lockChest.GetOrAdd(dimension, new object()))
             {
                 var affectedRows = 0;
                 long nextval;
                 int retries = 0;
 
-                do
+                using (var transaction = dbConnection.BeginTransaction())
                 {
-                    // Ensure we overwrite the value that has been read by this
-                    // instance in case another client is trying to lease a range
-                    // at the same time
-
-                    var transaction = session.Demand();
-                    
-                    var selectCommand = transaction.Connection.CreateCommand();
-                    selectCommand.CommandText = "SELECT " + _dialect.QuoteForColumnName("nextval") + " FROM " + _dialect.QuoteForTableName(_tablePrefix + TableName) + " WHERE " + _dialect.QuoteForTableName("dimension") + " = @dimension;";
-
-                    var selectDimension = selectCommand.CreateParameter();
-                    selectDimension.Value = dimension;
-                    selectDimension.ParameterName = "@dimension";
-                    selectCommand.Parameters.Add(selectDimension);
-
-                    selectCommand.Transaction = transaction;
-
-                    nextval = Convert.ToInt64(selectCommand.ExecuteScalar());
-
-                    var updateCommand = transaction.Connection.CreateCommand();
-                    updateCommand.CommandText = "UPDATE " + _dialect.QuoteForTableName(_tablePrefix + TableName) + " SET " + _dialect.QuoteForColumnName("nextval") + "=@new WHERE " + _dialect.QuoteForColumnName("nextval") + " = @previous AND " + _dialect.QuoteForColumnName("dimension") + " = @dimension;";
-
-                    var updateDimension = updateCommand.CreateParameter();
-                    updateDimension.Value = dimension;
-                    updateDimension.ParameterName = "@dimension";
-                    updateCommand.Parameters.Add(updateDimension);
-
-                    var newValue = updateCommand.CreateParameter();
-                    newValue.Value = nextval + _range;
-                    newValue.ParameterName = "@new";
-                    updateCommand.Parameters.Add(newValue);
-
-                    var previousValue = updateCommand.CreateParameter();
-                    previousValue.Value = nextval;
-                    previousValue.ParameterName = "@previous";
-                    updateCommand.Parameters.Add(previousValue);
-
-                    updateCommand.Transaction = transaction;
-
-                    affectedRows = updateCommand.ExecuteNonQuery();
-
-                    if (retries++ > MaxRetries)
+                    do
                     {
-                        throw new Exception("Too many retries while trying to lease a range for: " + dimension);
-                    }
+                        // Ensure we overwrite the value that has been read by this
+                        // instance in case another client is trying to lease a range
+                        // at the same time
 
-                } while (affectedRows == 0);
+                        using (var selectCommand = transaction.Connection.CreateCommand())
+                        {
+                            selectCommand.CommandText = _selectSql;
+
+                            var selectDimension = selectCommand.CreateParameter();
+                            selectDimension.Value = dimension;
+                            selectDimension.ParameterName = "@dimension";
+                            selectCommand.Parameters.Add(selectDimension);
+
+                            selectCommand.Transaction = transaction;
+
+                            nextval = Convert.ToInt64(selectCommand.ExecuteScalar());
+                        }
+
+                        using (var updateCommand = transaction.Connection.CreateCommand())
+                        {
+                            updateCommand.CommandText = _updateSql;
+
+                            var updateDimension = updateCommand.CreateParameter();
+                            updateDimension.Value = dimension;
+                            updateDimension.ParameterName = "@dimension";
+                            updateCommand.Parameters.Add(updateDimension);
+
+                            var newValue = updateCommand.CreateParameter();
+                            newValue.Value = nextval + _range;
+                            newValue.ParameterName = "@new";
+                            updateCommand.Parameters.Add(newValue);
+
+                            var previousValue = updateCommand.CreateParameter();
+                            previousValue.Value = nextval;
+                            previousValue.ParameterName = "@previous";
+                            updateCommand.Parameters.Add(previousValue);
+
+                            updateCommand.Transaction = transaction;
+
+                            affectedRows = updateCommand.ExecuteNonQuery();
+                        }
+
+                        if (retries++ > MaxRetries)
+                        {
+                            throw new Exception("Too many retries while trying to lease a range for: " + dimension);
+                        }
+                    } while (affectedRows == 0);
+
+                    // commit transaction
+                    transaction.Commit();
+
+                }//-- end using(var transaction...)
 
                 _increment = -1; // Start with -1 as it will be incremented
                 _start = nextval;
                 _end = nextval + _range - 1;
-                
             }
         }
 
@@ -120,46 +159,52 @@ namespace RdbmsLinearBlockIdGenerator
                 return;
             }
 
-            var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted);
-
-            
-            // Does the record already exist?
-            var selectCommand = transaction.Connection.CreateCommand();
-            selectCommand.CommandText = "SELECT " + _dialect.QuoteForColumnName("nextval") + " FROM " + _dialect.QuoteForTableName(_tablePrefix + TableName) + " WHERE dimension = @dimension;";
-
-            var selectDimension = selectCommand.CreateParameter();
-            selectDimension.Value = dimension;
-            selectDimension.ParameterName = "@dimension";
-            selectCommand.Parameters.Add(selectDimension);
-
-            selectCommand.Transaction = transaction;
-
-            var nextVal = selectCommand.ExecuteScalar();
-
-            if (null != nextVal)
+            using (var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted))
             {
-                return;
+                // Does the record already exist?
+                using (var selectCommand = transaction.Connection.CreateCommand())
+                {
+                    selectCommand.CommandText = _selectSql;
+
+                    var selectDimension = selectCommand.CreateParameter();
+                    selectDimension.Value = dimension;
+                    selectDimension.ParameterName = "@dimension";
+                    selectCommand.Parameters.Add(selectDimension);
+
+                    selectCommand.Transaction = transaction;
+
+                    var nextVal = selectCommand.ExecuteScalar();
+
+                    if (nextVal != null)
+                    {
+                        return;
+                    }
+                }
+
+                using (var command = transaction.Connection.CreateCommand())
+                {
+                    command.CommandText = _insertSql;
+
+                    var dimensionParameter = command.CreateParameter();
+                    dimensionParameter.Value = dimension;
+                    dimensionParameter.ParameterName = "@dimension";
+                    command.Parameters.Add(dimensionParameter);
+
+                    var nextValParameter = command.CreateParameter();
+                    nextValParameter.Value = 1;
+                    nextValParameter.ParameterName = "@nextval";
+                    command.Parameters.Add(nextValParameter);
+                    var x = command.Transaction == transaction;
+                    command.Transaction = transaction;
+
+                    command.ExecuteNonQuery();
+                }
+
+                // commit transaction
+                transaction.Commit();
             }
-
-            var command = transaction.Connection.CreateCommand();
-            command.CommandText = "INSERT INTO " + _dialect.QuoteForTableName(_tablePrefix + TableName) + " (" + _dialect.QuoteForColumnName("dimension") + ", " + _dialect.QuoteForColumnName("nextval") + ") VALUES(@dimension, @nextval);";
-
-            var dimensionParameter = command.CreateParameter();
-            dimensionParameter.Value = dimension;
-            dimensionParameter.ParameterName = "@dimension";
-            command.Parameters.Add(dimensionParameter);
-
-            var nextValParameter = command.CreateParameter();
-            nextValParameter.Value = 1;
-            nextValParameter.ParameterName = "@nextval";
-            command.Parameters.Add(nextValParameter);
-
-            command.Transaction = transaction;
-
-            command.ExecuteNonQuery();
 
             _initialized = true;
         }
     }
-
 }
