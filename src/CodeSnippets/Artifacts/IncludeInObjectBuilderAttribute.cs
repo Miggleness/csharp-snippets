619c5e3cd255162525afd2610a17e0acc812eb7a
Miguel Cudaihl <miguelc@x-pressfeeders.com>
2018-11-23 12:01:08 +0800

    Added artifact

---
+++ src/CodeSnippets/Artifacts/IncludeInObjectBuilderAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSnippets.Artifacts
+{
+    [System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    public sealed class IncludeInObjectBuilderAttribute : Attribute
+    {
+        readonly string name;
+
+        // This is a positional argument
+        public IncludeInObjectBuilderAttribute(string name)
+        {
+            this.name = name;
+
+            // TODO: Implement code here
+
+            throw new NotImplementedException();
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
